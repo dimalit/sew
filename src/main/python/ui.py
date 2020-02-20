12eb3c737bf7ff9f643caf54b6dc23ecef55f1e2
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-02-20 22:50:05 +0200

    Move account info to separate AccountWidget

--- src/main/python/ui.py
+++ src/main/python/ui.py
@@ -1,8 +1,38 @@
 from PyQt5.QtWidgets import *
 
+class AccountWidget(QGroupBox):
+    def __init__(self, title = "", parent = None):
+        QGroupBox.__init__(self, parent)
+        
+        self.setTitle(title or "Account:")
+        
+        self.layout = QGridLayout()
+        
+        self.private_key_edit = QLineEdit()
+        self.address_edit = QLineEdit()
+        self.transaction_count_edit = QLineEdit()
+        self.balance_edit = QLineEdit()
+        self.transaction_widget = TransactionWidget("Pending Transaction:")
+        
+        self.layout.addWidget(QLabel("Private Key:"), 0, 0)
+        self.layout.addWidget(self.private_key_edit, 0, 1)
+        self.layout.addWidget(QLabel("Address:"), 1, 0)
+        self.layout.addWidget(self.address_edit, 1, 1)
+        
+        self.hor_layout = QHBoxLayout()
+        self.hor_layout.addWidget(QLabel("Transaction Count:"))
+        self.hor_layout.addWidget(self.transaction_count_edit)
+        self.hor_layout.addWidget(QLabel("Balance:"))
+        self.hor_layout.addWidget(self.balance_edit)
+        self.hor_layout.addWidget(QLabel("ETH"))
+        
+        self.layout.addLayout(self.hor_layout, 2, 0, 1, 2)
+        
+        self.setLayout(self.layout)        
+
 class TransactionWidget(QGroupBox):
     def __init__(self, title = "", parent = None):
-        QWidget.__init__(self, parent)
+        QGroupBox.__init__(self, parent)
         
         self.setTitle(title or "Transaction:")
         
@@ -43,28 +73,12 @@ class WalletWidget(QWidget):
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
         
-        self.layout = QGridLayout()
+        self.layout = QVBoxLayout()
         
-        self.private_key_edit = QLineEdit()
-        self.address_edit = QLineEdit()
-        self.transaction_count_edit = QLineEdit()
-        self.balance_edit = QLineEdit()
+        self.account_widget     = AccountWidget()
         self.transaction_widget = TransactionWidget("Pending Transaction:")
-        
-        self.layout.addWidget(QLabel("Private Key:"), 0, 0)
-        self.layout.addWidget(self.private_key_edit, 0, 1)
-        self.layout.addWidget(QLabel("Address:"), 1, 0)
-        self.layout.addWidget(self.address_edit, 1, 1)
-        
-        self.hor_layout = QHBoxLayout()
-        self.hor_layout.addWidget(QLabel("Transaction Count:"))
-        self.hor_layout.addWidget(self.transaction_count_edit)
-        self.hor_layout.addWidget(QLabel("Balance:"))
-        self.hor_layout.addWidget(self.balance_edit)
-        self.hor_layout.addWidget(QLabel("ETH"))
-        
-        self.layout.addLayout(self.hor_layout, 2, 0, 1, 2)
-        
-        self.layout.addWidget(self.transaction_widget, 3, 0, 1, 2)
+                
+        self.layout.addWidget(self.account_widget)
+        self.layout.addWidget(self.transaction_widget)
         
         self.setLayout(self.layout)
